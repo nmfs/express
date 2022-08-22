73b137e1e1cf20176e74dc01f624287e4607aa0f
nmfs <nuno.freitas.silva@gmail.com>
2022-08-22 11:41:02 +0100

    3.14

--- index.js
+++ index.js
@@ -1,62 +1,42 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
+
 const app = express()
 
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
-/*
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
-*/
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
+  Person.findById(req.params.id).then(p => res.json(p))
+  /*const id = Number(req.params.id)
   const person = persons.find(p => p.id === id)
   if(person) {
     console.log(person)
     res.json(person)
   } else {
     res.status(404).end()
-  }
+  }*/
 })
 
+
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
+  Person.deleteOne({ _id: req.params.id})
+  /*
   console.log(`deleted id=${id}`)
   persons = persons.filter(p => p.id !== id)
-
+*/
   res.status(204).end()
 })
 
@@ -66,31 +46,27 @@ app.post('/api/persons', (req, res) => {
   if(!body.name || !body.number) {
     return res.status(400).json({ error: 'name or number missing'})
   }
-  if(persons.find(person => person.name === body.name)) {
-    return res.status(400).json({ error: 'name must be unique'})
-  }
 
-
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random()*1000000),
-  }
-  console.log(person)
-  persons = persons.concat(person)
+  })
 
-  res.json(person)
+  person.save().then(p => {
+    res.json(p)
+  })
 })
 
+/*
 app.get('/info', (req, res) => {
   res.send(`<div>Phonebook has info for ${persons.length} people</div>
       <br />
       <div>${new Date().toString()}</div>
 `)
 })
+*/
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
